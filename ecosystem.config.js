31970fd8e4785301070cbcb5bce947e7ca76b871
rolldeep-stepmerrily <rolldeep@stepmerrily.com>
2024-08-26 11:39:05 +0900

    update : CI/CD

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: 'github-follower-bot',
+      script: './dist/main.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+    },
+  ],
+};
